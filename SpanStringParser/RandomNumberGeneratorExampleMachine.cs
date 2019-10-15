69b60fc665efdf797875a4290f9ecfef4c98ac62
Karl T <karl.tillstrom@gmail.com>
2019-10-15 16:22:32 +0200

    Code from the stream

---
+++ SpanStringParser/RandomNumberGeneratorExampleMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpanStringParser
+{
+    class RandomNumberGeneratorExampleMachine
+    {
+        public async Task RandomGeneratorExample()
+        {
+            await foreach (var i in AsyncEnumerable.Skip<int>(RandomIntGeneratorOnTheInternet(), 10).Take(10))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public async IAsyncEnumerable<int> RandomIntGeneratorOnTheInternet()
+        {
+            while (true)
+            {
+                await Task.Delay(100);
+                yield return new Random().Next();
+            }
+        }
+    }
+}
